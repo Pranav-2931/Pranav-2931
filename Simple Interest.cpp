711eec55744b9b031d14962b012399ff2f79f25f
Pranav Raj <pranav.2007.2931@gmail.com>
2024-10-15 09:44:54 +0530

    Add files via upload

---
+++ Simple Interest.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{int P, R, T,SI;
+    printf("Enter the principal: ");
+    scanf("%d", &P);
+    printf("Enter the rate: ");
+    scanf("%d", &R);
+    printf("Enter the time: ");
+    scanf("%d", &T);
+    SI = (P * R * T) / 100;
+    printf("The Simple interest is %d", SI);
+    return 0;
+}
